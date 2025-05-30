fa3550a124313e7591eeea458910b146a92265a4
Jim Mason <jmason@ibinx.com>
2025-05-30 11:13:59 +0100

    Apache mod_evasive 2.4.0 with modular configuration

---
+++ components/apache2-modules/mod_evasive/files/build.mk
@@ -0,0 +1,39 @@
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2025 Jim Mason <jmason at ibinx doc com>.
+#
+# Adapted from original code Copyright (c) 2011 Oracle and/or its affiliates.
+#
+
+APXS=apxs
+
+CC=`$(APXS) -q CC`
+CFLAGS=`$(APXS) -q CFLAGS`
+
+all: .libs/mod_evasive.so
+
+.libs/mod_evasive24.o: mod_evasive24.c
+	$(APXS) -c -o mod_evasive.so mod_evasive24.c && rm .libs/mod_evasive.so
+
+.libs/mod_evasive.so: .libs/mod_evasive24.o
+	$(CC) $(CFLAGS) -shared -o .libs/mod_evasive.so .libs/mod_evasive24.o -lsocket -lnsl
